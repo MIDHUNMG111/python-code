28c63a757925291d00ea1a5157010e949028d485
MIDHUNMG111 <mmidhunmanikandan@gmail.com>
2025-03-07 15:10:11 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,12 @@
+string = input()
+vowels = ['a','e','i','o','u']
+usr = input().lower()
+count = 0
+for i in usr:
+    if i in vowels:
+        count+=1
+        
+if count == 0:
+    print("No vowels were found.")
+else:
+    print(f"Total number of vowels: {count}")
